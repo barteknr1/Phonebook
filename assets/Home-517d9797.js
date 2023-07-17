69ecc7d67093dbb0cd15cc5f4a47595495c3adf2
Bartek <Barteknr1@gmail.com>
2023-07-17 19:04:29 +0200

    Updates

---
+++ assets/Home-517d9797.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-46e074fa.js";const r=()=>o.jsx("div",{children:o.jsx("h1",{children:"Welcome to Phonebook"})});export{r as default};
--- assets/Home-dd9c5c2a.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-423903c1.js";const r=()=>o.jsx("div",{children:o.jsx("h1",{children:"Welcome to Phonebook"})});export{r as default};
