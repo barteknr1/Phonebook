0fb94f0438d925e85766f8f18107617130788748
Bartek <Barteknr1@gmail.com>
2023-07-18 21:43:57 +0200

    Updates

--- assets/Home-b599567e.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-4dd42b0f.js";import{q as o,W as r}from"./index.module-6f04d8ea.js";const s=()=>e.jsxs(e.Fragment,{children:[e.jsx(o,{children:e.jsx(r,{children:e.jsx("title",{children:"Welcome to Phonebook"})})}),e.jsx("div",{children:e.jsx("h1",{children:"Welcome to Phonebook"})})]});export{s as default};
