de78126abb97027d56c52e849e1888d1825d16f1
barteknr1 <barteknr1@gmail.com>
2024-03-25 19:02:32 +0100

    Updates

--- assets/Home-6d880f84.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-86abc9c9.js";import{q as o,W as s}from"./index.module-d8fcfdf3.js";const c="_welcome_13b60_1",l={welcome:c},m=()=>e.jsxs(e.Fragment,{children:[e.jsx(o,{children:e.jsx(s,{children:e.jsx("title",{children:"Welcome to Phonebook"})})}),e.jsx("div",{className:l.welcome,children:e.jsx("h1",{children:"Welcome to Phonebook"})})]});export{m as default};
